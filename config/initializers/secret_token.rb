1fcc382e12f3b6cc2adf0e4b27c98809b0c43175
Fharzana Elankumaran <fharzana@gmail.com>
2012-02-20 17:15:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iheartcurry::Application.config.secret_token = 'af1dd57b173057100ff35556e34debc4b3fe0fddb295a0b83f90cd7a30cb48371f1a70ceefda27aa87a41285eafabbb2d8c787994bb16eb81d25e33fcf178861'
